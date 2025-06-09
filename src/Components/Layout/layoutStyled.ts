bdc26b0fa49982bcde8c556db40bc3c04cd10ab5
matty199905 <matty199905@gmail.com>
2025-06-09 15:18:39 -0300

    final

--- src/Components/Layout/layoutStyled.ts
+++ src/Components/Layout/layoutStyled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import LayoutImg from '../../Imgs/Layout/Layout.avif'
-import Hero from '../../Imgs/Hero/wallpaper.jpg'
+
 
 
 type LayoutProps = {
@@ -10,30 +10,21 @@ type LayoutProps = {
 }
 
 export const LayoutWrapper = styled.div<LayoutProps>`
-  display: flex;
-  flex-direction: column;
-  justify-content: ${(props)=>(props.dinamicPage ? 'center' : 'flex-start')};
-  align-items: center;
-  width: 100%;
-  min-height: 100vh;
-  background-image: ${(props)=>(props.layoutWallpaper ? `url(${LayoutImg})` : props.homeWallpaper ? `url(${Hero})` : 'none')};
-  background-attachment: fixed;
-  background-size: cover;
-  background-position-y: -540px;
-  z-index: 0;
-  
-  @media (min-width: 550px){
-      background-image:  ${(props)=>(props.dinamicPage ? `url(${LayoutImg})`  : undefined)};
-  }
-    @media (max-width: 1200px){
-  background-position-y: -350px;
-  }
-  @media (max-width: 890px){
-  background-position-y: -180px;
-  }
-    @media (max-width: 600px){
-  background-position-y: 0px;
-  }
+position: relative;
+display: flex;
+flex-direction: column;
+justify-content:  ${(props)=>(props.dinamicPage ? 'center' : 'flex-start')};
+align-items: center;
+width: 100%;
+min-height: 100dvh;
+height: auto;
+background-image:${(props)=>(props.layoutWallpaper ? `url(${LayoutImg})` : props.homeWallpaper ? `url(${LayoutImg})` :  'none')};
+background-attachment:fixed ;
+background-size: cover;
+z-index: 0;
+@media (min-width: 550px){
+    background-image:  ${(props)=>(props.dinamicPage ? `url(${LayoutImg})`  : undefined)};
+}
 `
 export const Overlay = styled.div<LayoutProps>`
 position: absolute;
