2b6038eeee5ea0bf89db4a09fc3f046798d900ae
matty199905 <matty199905@gmail.com>
2025-07-11 16:26:02 -0300

    final

--- src/Components/Layout/layoutStyled.ts
+++ src/Components/Layout/layoutStyled.ts
@@ -17,11 +17,27 @@ export const LayoutWrapper = styled.div<LayoutProps>`
   width: 100%;
   min-height: 100vh; 
   height: auto;
-  background-image:${(props)=>(props.layoutWallpaper ? `url(${LayoutImg})` : props.homeWallpaper ? `url(${Hero})` : undefined)};
-  background-size: cover;
   z-index: 0;
-  @media (min-width: 550px){
-    background-image:  ${(props)=>(props.dinamicPage ? `url(${LayoutImg})`  : undefined)};
+  &::before {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+    pointer-events: none;
+      background-image:${(props)=>(props.layoutWallpaper ? `url(${LayoutImg})` : props.homeWallpaper ? `url(${Hero})` : undefined)};
+  background-size: cover;
+  background-position: center;
+  padding-top: 500px;
+  background-repeat: no-repeat;
+  @media (max-width: 850px) {
+   padding-top: 200px
+}
+ @media (min-width: 550px){
+   background-image:  ${(props)=>(props.dinamicPage ? `url(${LayoutImg})`  : undefined)};
+ }
   }
 `
 export const Overlay = styled.div<LayoutProps>`
